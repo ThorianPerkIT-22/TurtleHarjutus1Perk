43697eea9fe53bd55266c0ac1bb75a187d9b2b47
ThorianPerkIT-22 <115464923+ThorianPerkIT-22@users.noreply.github.com>
2022-10-10 13:22:49 +0300

    Add files via upload

---
+++ Harjutus1Perk2.py
@@ -0,0 +1,73 @@
+#thorianperk
+#10.10.22
+#harjutame kilpkonna
+import turtle
+
+#tekitame akna
+aken = turtle.Screen()
+aken.setup(300,300)
+aken.title("ülesanne 1 - MM")
+
+#kilpkonnade loomine
+k1=turtle.Turtle()
+k2=turtle.Turtle()
+k3=turtle.Turtle()
+k4=turtle.Turtle()
+k5=turtle.Turtle()
+k6=turtle.Turtle()
+k7=turtle.Turtle()
+k8=turtle.Turtle()
+
+#Värvide seadistamine
+k1.color("lime")
+k2.color("red")
+k3.color("black")
+k4.color("gold")
+k5.color("silver")
+k6.color("blue")
+k7.color("pink")
+k8.color("orange")
+
+#kilpkonnade liigutamine
+k1.lt(0)
+k1.fd(100)
+k2.lt(45)
+k2.fd(100)
+k3.lt(90)
+k3.fd(100)
+k4.lt(135)
+k4.fd(100)
+k5.lt(180)
+k5.fd(100)
+k6.lt(225)
+k6.fd(100)
+k7.lt(270)
+k7.fd(100)
+k8.lt(315)
+k8.fd(100)
+
+
+
+
+
+
+"""
+
+#teeme kilpkonna
+taavi = turtle.Turtle()
+#liigutame kilpkonna
+taavi.color("green")
+taavi.forward(150)
+taavi.left(90)
+taavi.fd(100) #forward
+taavi.lt(90) #left
+taavi.fd(150)
+taavi.lt(90)
+taavi.fd(100)
+
+
+
+#suleb akna klikkides
+turtle.exitonclick()
+
+"""
